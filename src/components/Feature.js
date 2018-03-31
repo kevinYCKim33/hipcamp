288458ec43b204cb617062d626f387722e4e327c
Kevin Yongchul Kim <kevikim33@gmail.com>
2018-03-31 03:07:04 -0700

    Feature handles case where subfeatures do not exist

--- src/components/Feature.js
+++ src/components/Feature.js
@@ -3,16 +3,42 @@ import React from 'react';
 class Feature extends React.Component {
   constructor() {
     super();
+    this.handleOnClick = this.handleOnClick.bind(this);
+    this.state = {
+      currentSubfeatures: null,
+      message: "",
+    }
   }
 
   handleOnClick = () => {
-    debugger;
+    if (this.props.feature.subfeatures.length > 0) {
+      this.setState({
+        currentSubfeatures: this.props.feature.subfeatures,
+      });
+    } else {
+      this.setState({
+        message: "No subfeatures exist",
+      });
+    }
   }
 
   render() {
+    const rNow = this.state.currentSubfeatures;
+    const code = this.state.message;
     return (
-      <li onClick={this.handleOnClick} className={ this.props.feature.presence ? null : "unavailable" }>
+      <li
+        onClick={this.handleOnClick}
+        className={ this.props.feature.presence ? null : "unavailable" }
+      >
         {this.props.feature.title}
+        <br />
+        {this.state.message !== ""  &&
+          <ul>
+            <li>
+              No subfeatures to display
+            </li>
+          </ul>
+        }
       </li>
     )
   }
