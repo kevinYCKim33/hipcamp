2d6a1274859de9015317d0534521894e3422d60f
Kevin Yongchul Kim <kevikim33@gmail.com>
2018-04-02 00:51:05 -0700

    extracted out jsx if statements in render's return

--- src/components/Feature.js
+++ src/components/Feature.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import FeaturesList from './FeaturesList';
+import NoSubFeature from './NoSubFeature';
 
 class Feature extends React.Component {
   constructor() {
     super();
     this.handleOnClick = this.handleOnClick.bind(this);
     this.state = {
-      currentSubfeatures: null,
+      subfeatures: null,
       message: "",
     }
   }
 
   handleOnClick = () => {
-    if (this.props.feature.subfeatures.length > 0) {
+    const {subfeatures} = this.props.feature;
+    if (subfeatures.length > 0) {
       this.setState({
-        currentSubfeatures: this.props.feature.subfeatures,
+        subfeatures
       });
     } else {
       this.setState({
@@ -24,25 +26,31 @@ class Feature extends React.Component {
   }
 
   render() {
-    const rNow = this.state.currentSubfeatures;
-    const code = this.state.message;
+    const {feature} = this.props;
+    const {subfeatures, message} = this.state;
+
+    let dne = null;
+    if (message !== "") {
+      dne = (
+        <NoSubFeature/>
+      )
+    }
+    let thing = null;
+    if (subfeatures !== null) {
+      thing = (
+        <FeaturesList features={subfeatures}/>
+      )
+    }
     return (
       <li
         onClick={this.handleOnClick}
-        className={ this.props.feature.presence ? null : "unavailable" }
+        className={ feature.presence ? null : "unavailable" }
       >
-        {this.props.feature.title}
-        <br />
-        {this.state.message !== ""  &&
-          <ul>
-            <li>
-              No subfeatures to display
-            </li>
-          </ul>
-        }
-        {this.state.currentSubfeatures !== null &&
-          <FeaturesList features = {this.state.currentSubfeatures}/>
-        }
+        <div>
+          {feature.title}
+        </div>
+        {dne}
+        {thing}
       </li>
     )
   }
---
+++ src/components/NoSubFeature.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const NoSubFeature = () => {
+  return (
+    <ul>
+      <li>
+        No subfeatures to display
+      </li>
+    </ul>
+  )
+}
+
+export default NoSubFeature;
